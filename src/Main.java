e3421604d10229dfd1c87c90b32f4578164323ef
Max Shvagerus <mshvagerus@gmail.com>
2022-10-06 17:15:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 250; //цена билета
+        int perMiles = 20; // количество рублей за 1 милю
+
+        int bonusMiles = ticketPrice / perMiles;
+
+        System.out.println(bonusMiles);
+    }
+}
