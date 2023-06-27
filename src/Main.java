f58414826c56d5dc474ced733701b69d1650f77b
Max Shvagerus <mshvagerus@gmail.com>
2023-06-28 01:53:08 +0300

    add javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
+    /**
+     * За каждые 20 рублей, потраченные на билет, начисляется 1 миля. Дробные мили не начисляются.
+     * @value ticketPrice - цена билета.
+     * @value perMiles - количество рублей за 1 милю.
+     */
     public static void main(String[] args) {
 
-        int ticketPrice = 250; //цена билета
-        int perMiles = 20; // количество рублей за 1 милю
+        int ticketPrice = 250;
+        int perMiles = 20;
 
         int bonusMiles = ticketPrice / perMiles;
 
